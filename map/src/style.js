ae0e4489fa38ed920f41b2f154a780f9cd74edf3
老猫 <124508738+Laomao1104@users.noreply.github.com>
2023-04-23 15:58:07 +0800

    Bug
    https://laomao1104.github.io/map/about/

--- map/src/style.js
+++ map/src/style.js
@@ -6,7 +6,7 @@
 	header>.nav{position:absolute;width:max(32ex,33%);max-height:calc(calc(100vh - 100%) - 16px);overflow:auto;transform:translateX(-5%);transition:.2s;opacity:0;visibility:hidden;pointer-events:none;display:grid;grid-template-columns:repeat(auto-fill,minmax(min(12ex,100%),1fr));gap:var(--bor);}
 	header>.nav>*{text-align:center;margin:0;}
 	header>.navb:hover+*,header>.navb:focus+*,.nav:hover,.nav:focus-within{transform:none;opacity:1;visibility:visible;pointer-events:auto;}
-</style><header><button class="zab bgca navb">老猫的网站</button><nav class="zab nav">/index.html
-,主页;..about/,关于;..map/,地图;..news/,快讯;..bulidings/,建筑;..server/,伺服器;..function/,指令包;
+</style><header><button class="zab bgca navb">老猫的网站</button><nav class="zab nav">../index.html
+,主页;../about/,关于;../map/,地图;../news/,快讯;../bulidings/,建筑;../server/,伺服器;../function/,指令包;
 github.com/laomao1104,GitHub;facebook.com,Fb;youtube.com/@laomao234,YouTube;
 </nav></header>`.replace(/(.*?),(.*?);/g,(_,x,y,p)=>`<a class="zab bgca" href="${p<60?root:'https://'}${x}">${y}</a>`)],['beforeend',`<hr><footer>2024 老猫｡<br></footer>`]].map(x=>d.body.insertAdjacentHTML(...x)))))(document);
